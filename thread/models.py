112f12bced290b993fef159be9cc0ebd22889f93
amaechijude <ugwuamaechi5@gmail.com>
2024-01-31 20:43:08 +0100

    profile

--- thread/models.py
+++ thread/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
-from django.contrib.auth import get_user_model
-# Create your models here
-#get current user
-User = get_user_model()
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)

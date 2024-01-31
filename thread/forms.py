c498e9ebb4d78b3fc510f7e1fe9cd32b1f291823
amaechijude <ugwuamaechi5@gmail.com>
2024-01-31 22:38:52 +0100

    Added update profile form

--- thread/forms.py
+++ thread/forms.py
@@ -7,3 +7,9 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email','password1', 'password2')
+
+
+class UpdateProfile(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'id_user')
--- thread/models.py
+++ thread/models.py
@@ -8,9 +8,11 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(max_length=1000, blank=True)
     profile_pics = models.ImageField(upload_to='profile_images', default='anon.png')
-    location_city = models.CharField(max_length=100, blank=user)
+    location_city = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return (f"{self.user.username}")

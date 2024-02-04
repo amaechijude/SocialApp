d3aad2e0d7eb7e288294531f33f82e3f8e4a14fd
amaechijude <ugwuamaechi5@gmail.com>
2024-02-04 21:15:49 +0100

    created post form

--- thread/forms.py
+++ thread/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
@@ -13,3 +13,9 @@ class UpdateProfile(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'bio', 'profile_pics', 'location_city')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('postID', 'author', 'created_at',)

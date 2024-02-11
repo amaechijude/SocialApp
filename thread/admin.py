0ce092d2053e6eb5ad2a33c33445be60839fb4b0
amaechijude <ugwuamaechi5@gmail.com>
2024-02-11 16:30:13 +0100

    followers

--- thread/admin.py
+++ thread/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, PostModel, LikePost
+from .models import Profile, PostModel, LikePost, FollowerModel
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(PostModel)
 admin.site.register(LikePost)
+admin.site.register(FollowerModel)

fce489f64b577a4e25273c3c911361c4a3b4b0c8
amaechijude <ugwuamaechi5@gmail.com>
2024-02-25 08:44:19 +0100

    liked by

--- thread/models.py
+++ thread/models.py
@@ -9,7 +9,7 @@ User = get_user_model()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    id_user = models.AutoField(primary_key=True)
+    id_user = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(max_length=1000, blank=True)

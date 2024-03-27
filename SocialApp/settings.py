2973ca60baa819988beb6c8bd46396991744e778
amaechijude <ugwuamaechi5@gmail.com>
2024-03-27 13:08:06 +0100

    env

--- SocialApp/settings.py
+++ SocialApp/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY =django-insecure-ob#0#_*$kyc*!i2l^o+&)r)q+9e3y02u1yk^##(uy3f$05ddm5
+SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY =django-insecure-ob#0#_*$kyc*!i2l^o+&)r)q+9e3y02u1yk^##(uy3f$05ddm5
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', cast=bool)
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
+#DEBUG = False
 
 #Change allowed hosts in production
 ALLOWED_HOSTS = ['*']

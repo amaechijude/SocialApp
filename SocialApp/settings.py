e46a78973dddd0e361c1bc85f8420b0c74c752d3
amaechijude <ugwuamaechi5@gmail.com>
2024-04-06 02:53:46 +0100

    push

--- SocialApp/settings.py
+++ SocialApp/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'cloudinary_storage',
     'cloudinary',
     'rest_framework',
